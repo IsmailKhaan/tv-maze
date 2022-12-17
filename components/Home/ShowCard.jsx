da49d48c4f78d6c6cdde7cb55b52e1bc69e2b733
ismailkhan-07 <ismailkhan358391@gmail.com>
2022-12-18 03:59:18 +0500

    feat : (blur loading image)

--- components/Home/ShowCard.jsx
+++ components/Home/ShowCard.jsx
@@ -1,13 +1,22 @@
 import Image from 'next/image'
 import React from 'react'
 import StarRating from '../StarRating/StarRating'
+import convertImage from '../utils/blur-bg'
 import { showCardHover } from './styles/HomeStyle'
 
 const ShowCard = ({ key, image, summary, rating }) => {
-    
+
+    const toBase64 = (str) =>
+        typeof window === 'undefined'
+            ? Buffer.from(str).toString('base64')
+            : window.btoa(str);
+
     return (
         <div className={showCardHover}>
-            <Image  className='rounded object-cover' width={200} height={250} src={image} alt='showcover' layout="responsive" loading="lazy" unobserve />
+            <Image className='rounded object-cover' placeholder='blur' blurDataURL={`data:image/svg+xml;base64,${toBase64(
+                convertImage(700, 475)
+            )}`}
+                width={200} height={250} src={image} alt='' layout="responsive" loading="lazy" unobserve />
 
             <div className='flex items-center gap-1 text-[14px] text-gray-500'>
                 <StarRating key={key} rating={rating} />
